c11ec3bae9386998734f050f8ff820c7a7ea4175
Alejandro Tonatiuh <alexsarget@gmail.com>
2019-07-30 13:24:37 -0500

    programa inicial. tengo flojera

--- demo1.ino
+++ demo1.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.println("Inicializando programa");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+Serial.println("tengo flojera de programar");
+while (1){Serial.print(".");
+delay(5000); 
+}
 }
