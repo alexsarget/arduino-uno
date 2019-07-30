8e76274a25c08953ac5e0d9e467d642a07530469
Alejandro Tonatiuh <alexsarget@gmail.com>
2019-07-30 14:09:57 -0500

    actualizacion del demo

--- demo1.ino
+++ demo1.ino
@@ -1,14 +1,20 @@
+int PINOUT=2;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 Serial.println("Inicializando programa");
+Serial.println ("Setting pin 2 as output");
+pinMode (PINOUT,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println("tengo flojera de programar");
-while (1){Serial.print(".");
-delay(5000); 
-}
+Serial.println("PIN 2 -> ON");
+digitalWrite (PINOUT,HIGH);
+delay (500);
+Serial.println("PIN 2 -> OFF");
+digitalWrite (PINOUT,LOW);
+delay (500); 
+
 }
